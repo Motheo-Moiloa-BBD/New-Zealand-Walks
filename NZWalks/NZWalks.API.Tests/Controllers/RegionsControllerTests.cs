6697193d534215749407466e2daf40a727ecfe04
Motheo-Moiloa-BBD <motheo@bbd.co.za>
2024-06-03 11:59:09 +0200

    Add post async unit test

--- NZWalks/NZWalks.API.Tests/Controllers/RegionsControllerTests.cs
+++ NZWalks/NZWalks.API.Tests/Controllers/RegionsControllerTests.cs
@@ -1,13 +1,16 @@
 ﻿
+using Azure;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Newtonsoft.Json;
+using NZWalks.Core.Exceptions;
 using NZWalks.Core.Models.DTO;
 using NZWalks.Services.Interfaces;
 using System.Net;
+using System.Net.Http.Json;
 using System.Security.Claims;
 
 
@@ -83,5 +86,78 @@ namespace NZWalks.API.Tests.Controllers
             });
         }
 
+        [Fact]
+        public async Task GetById_Success()
+        {
+            //Arrange 
+            var id = Guid.Parse("9710f419-cc01-4489-adc0-ad43d529cdbe");
+            var existingRegion = new RegionDTO()
+            {
+                Id = Guid.Parse("9710f419-cc01-4489-adc0-ad43d529cdbe"),
+                Code = "EMB",
+                Name = "Embalenhle",
+                RegionImageUrl = "embalenhle-image.png"
+            };
+
+            regionServiceMock.Setup(regionService => regionService.GetRegionById(id)).ReturnsAsync(existingRegion);
+
+            //Act
+            var response = await _httpClient.GetAsync($"api/regions/{id}");
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            var returnedJson = await response.Content.ReadAsStringAsync();
+            var returnedRegion = JsonConvert.DeserializeObject<RegionDTO>(returnedJson);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equivalent(existingRegion, returnedRegion, strict: true);
+        }
+
+        [Fact]
+        public Task GetById_NotFoundException_404()
+        {
+            return AssertThatGetByIdHandlesGivenException(new NotFoundException("Region not found."), HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Create_Success()
+        {
+            //Arrange
+            var addRegion = new AddRegionDTO()
+            {
+                Code = "MAH",
+                Name = "Mahikeng",
+                RegionImageUrl = "mahikeng-image.png"
+            };
+
+            var expectedRegion = new RegionDTO()
+            {
+                Id = Guid.Parse("9710f419-cc01-4489-adc0-ad43d529cdbe"),
+                Code = "MAH",
+                Name = "Mahikeng",
+                RegionImageUrl = "mahikeng-image.png"
+            };
+
+            regionServiceMock.Setup(regionService => regionService.CreateRegion(addRegion)).ReturnsAsync(expectedRegion);
+
+            //Act
+            var response = await _httpClient.PostAsync("api/regions", JsonContent.Create(addRegion));
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            var returnedJson = await response.Content.ReadAsStringAsync();
+            var returnedRegion = JsonConvert.DeserializeObject<RegionDTO>(returnedJson);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        private async Task AssertThatGetByIdHandlesGivenException(Exception givenException, HttpStatusCode resultingStatusCode)
+        {
+            var id = Guid.Parse("9710f419-cc01-4489-adc0-ad43d529cdbe");
+
+            regionServiceMock.Setup(regionService => regionService.GetRegionById(id)).ThrowsAsync(givenException);
+
+            var response = await _httpClient.GetAsync($"api/regions/{id}");
+
+            Assert.Equal(resultingStatusCode,response.StatusCode);
+        }
     }
 }
